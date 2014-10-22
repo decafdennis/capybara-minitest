c878e3284388682f9061ff427a8ea6373c944146
Dennis Stevense <dennis@naquah.net>
2014-10-22 13:01:20 -0700

    Add code climate test coverage reporting

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,11 @@ gem 'minitest'
 require 'minitest/autorun'
 require 'capybara'
 
+unless ENV['CODECLIMATE_REPO_TOKEN'].nil?
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
+
 Capybara.app = Proc.new do |env|
   [ 200, {'Content-Type' => 'text/html'}, ['Hello World']]
 end
