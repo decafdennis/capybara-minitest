c49a64c26bad3437947e4617432da2410ea3ec0a
Dennis Stevense <dennis@naquah.net>
2014-10-22 15:22:27 -0700

    Improve inline docs

--- lib/capybara/minitest/assertions/matcher.rb
+++ lib/capybara/minitest/assertions/matcher.rb
@@ -21,8 +21,10 @@ module Capybara
           name.to_s.match(/^have_/)
         end
 
+        # The name of the RSpec matcher, e.g. have_text.
         attr_reader :name
 
+        # Initializes a new matcher based on the given RSpec matcher name.
         def initialize(name)
           unless self.class.recognizes_name?(name)
             fail ArgumentError, 'Unrecognized matcher name.'
@@ -31,6 +33,11 @@ module Capybara
           @name = name.to_s
         end
 
+        # Returns the matcher's name in third person.
+        #
+        #   matcher.name              # => have_text (infinitive form)
+        #   matcher.third_person_name # => has_text  (third person form)
+        #
         def third_person_name
           @third_person_name ||= @name.gsub(/^have_/, 'has_')
         end
@@ -50,55 +57,66 @@ module Capybara
 
       # Represents an assertion based on a Capybara RSpec matcher.
       class MatcherAssertion
+        # The matcher this assertion is based on.
         attr_reader :matcher
 
+        # Initializes a new assertion with the given matcher.
         def initialize(matcher)
           @matcher = matcher
         end
 
+        # The name of the assertion.
+        #
+        #   assertion.matcher.name # => have_text
+        #   assertion.name         # => assert_has_text
+        #
         def name
-          "#{name_prefix}_#{@matcher.third_person_name}"
+          "#{self.class.name_prefix}_#{@matcher.third_person_name}"
         end
 
+        # Executes the assertion by testing it against the matcher on the given
+        # test subject and with the provided arguments. Returns an array with
+        # the test result (as a boolean) and the failure message.
         def test(subject, *args)
           # Create an RSpec matcher object.
           matcher = @matcher.matcher(*args)
           # Perform the match.
-          test_result = matcher.send(matcher_test_method, subject)
+          test_result = matcher.send(self.class.matcher_test_method, subject)
           # Get the failure message, which has to be done after the matching.
-          failure_message = matcher.send(matcher_failure_message_method)
+          failure_message = matcher.send(self.class.matcher_failure_message_method)
 
           [test_result, failure_message]
         end
 
-        protected
-
-        def name_prefix
+        # The prefix for method names of this kind of assertion.
+        def self.name_prefix
           'assert'
         end
 
-        def matcher_test_method
+        # The test method called on the RSpec matcher for this kind of
+        # assertion.
+        def self.matcher_test_method
           :matches?
         end
 
-        def matcher_failure_message_method
+        # The failure message method called on the RSpec matcher for this kind
+        # of assertion.
+        def self.matcher_failure_message_method
           :failure_message
         end
       end
 
       # Represents a refutation based on a Capybara RSpec matcher.
       class MatcherRefutation < MatcherAssertion
-        protected
-
-        def name_prefix
+        def self.name_prefix
           'refute'
         end
 
-        def matcher_test_method
+        def self.matcher_test_method
           :does_not_match?
         end
 
-        def matcher_failure_message_method
+        def self.matcher_failure_message_method
           :failure_message_when_negated
         end
       end
