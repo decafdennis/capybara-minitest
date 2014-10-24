89d13198cb19479dec728470639fb2a99a7f7fcc
Dennis Stevense <dennis@naquah.net>
2014-10-23 23:00:13 -0700

    Clean up long namespaces in matcher test

--- test/matcher_test.rb
+++ test/matcher_test.rb
@@ -1,21 +1,27 @@
 require 'test_helper'
 require 'capybara/minitest/assertions/matcher'
 
-# Tests Capybara::Minitest::Assertions::Matcher.
-class MatcherTest < Minitest::Test
-  def test_all_does_not_include_become_closed
-    refute Capybara::Minitest::Assertions::Matcher.all.any? { |matcher|
-      matcher.name == 'become_closed'
-    }
-  end
+module Capybara
+  module Minitest
+    module Assertions
+      # Tests Capybara::Minitest::Assertions::Matcher.
+      class MatcherTest < ::Minitest::Test
+        def test_all_does_not_include_become_closed
+          refute Matcher.all.any? { |matcher|
+            matcher.name == 'become_closed'
+          }
+        end
 
-  def test_does_not_recognize_become_closed
-    refute Capybara::Minitest::Assertions::Matcher.recognizes_name?('become_closed')
-  end
+        def test_does_not_recognize_become_closed
+          refute Matcher.recognizes_name?('become_closed')
+        end
 
-  def test_initialize_with_unrecognized_name
-    assert_raises ArgumentError do
-      Capybara::Minitest::Assertions::Matcher.new('become_closed')
+        def test_initialize_with_unrecognized_name
+          assert_raises ArgumentError do
+            Matcher.new('become_closed')
+          end
+        end
+      end
     end
   end
 end
